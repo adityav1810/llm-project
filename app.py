50416757d7dc1ee17c68fc9253a182f031a8a353
Aditya Varshney <43772912+adityav1810@users.noreply.github.com>
2025-04-15 15:25:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -338,12 +338,42 @@ def get_financial_answer(user_question: str, pdf_files: Union[List, None] = None
         "pdf_files": pdf_files
     })
 
-    return result.get("final_answer", "No answer returned.")
+    final = result.get("final_answer", {})
+    if isinstance(final, dict) and "content" in final:
+        return final["content"]
+    return final if isinstance(final, str) else str(final)
 
 
 # call streamlit 
 
-st.title("📄 PDF Q&A App")
+st.title("AI Agent for Financial Analysis")
+
+url = "https://github.com/adityav1810/llm-project/raw/main/"
+
+# Define the directory and filename
+directory = "data"
+# filenames = ["synthetic_stock_data.csv","Apple_quarterly_report.pdf","tesla_quarterly_report.pdf","Microsoft_10Q.pdf"]
+filenames = ["synthetic_stock_data.csv"]
+
+
+# Create directory if it doesn't exist
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    print(f"Directory '{directory}' created.")
+else:
+    print(f"Directory '{directory}' already exists.")
+
+for filename in filenames:
+    filepath = os.path.join(directory, filename)
+    # Download the CSV file
+    response = requests.get(url + filepath)
+
+
+    # Save the file to the directory
+    with open(filepath, "wb") as f:
+        f.write(response.content)
+        print(f"File saved to {filepath}")
+
 
 uploaded_files = st.file_uploader("Upload 3 PDF files", type="pdf", accept_multiple_files=True)
 question = st.text_input("Ask a question based on the PDFs")
